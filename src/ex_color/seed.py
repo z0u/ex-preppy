d5d1d493cd044a2c83f3ea163fd5515544c492a1
Alex Fraser <alex.d.fraser@gmail.com>
2025-07-24 03:26:35 +0000

    using Lightning's seed_everything

--- src/ex_color/seed.py
+++ src/ex_color/seed.py
@@ -1,25 +1,13 @@
 import logging
-import random
 
-import numpy as np
+import lightning as L
 import torch
 
 log = logging.getLogger(__name__)
 
 
-def seed_everything(seed: int):
-    """Set seeds for reproducibility."""
-    random.seed(seed)
-    np.random.seed(seed)
-    torch.manual_seed(seed)
-    if torch.cuda.is_available():
-        torch.cuda.manual_seed_all(seed)
-    log.info(f'Global random seed set to {seed}')
-
-
 def set_deterministic_mode(seed: int):
     """Make experiments reproducible."""
-    seed_everything(seed)
-    torch.backends.cudnn.deterministic = True
-    torch.backends.cudnn.benchmark = False
+    L.seed_everything(seed)
+    torch.use_deterministic_algorithms(True, warn_only=True)
     log.info('PyTorch set to deterministic mode')
