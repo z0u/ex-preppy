aa254590c4722137cc003a5edd72dac299094d82
Alex Fraser <alex.d.fraser@gmail.com>
2025-08-16 08:50:41 +0000

    improve intervention functions for repulsion

--- src/ex_color/intervention/falloff.py
+++ src/ex_color/intervention/falloff.py
@@ -1,46 +1,66 @@
-from typing import override
+from typing import Annotated, override
 
+from pydantic import Field, validate_call
 from torch import Tensor
+
+ZeroToOne = Annotated[float, Field(ge=0, le=1)]
 
 
 class Falloff:
     """
-    Adjust intervention amount based on distance to subject.
+    Determine intervention amount based on distance to subject.
 
     Args:
-        dots: Cosine distances of the activations to the subject, of shape [B].
+        alignment: Closeness of activations from the subject, of shape [B],
+        where 1 is "perfectly aligned" and 0 is "not at all aligned". This could
+        be cosine distance or some other measure.
 
     Returns:
-        The target cosine distances, of shape [B].
+        The offset from the original distance, of shape [B].
     """
 
-    def __call__(self, dots: Tensor, /) -> Tensor: ...
+    def __call__(self, alignment: Tensor, /) -> Tensor: ...
 
 
 class Linear(Falloff):
-    """Linear decay: target_dot = original_dot * decay_factor"""
+    """
+    Linear decay: d.decay
 
-    def __init__(self, decay: float = 0.5):
-        assert 0 <= decay <= 1, 'decay_factor must be in [0, 1]'
+    For cosine distance: produces a circular lobe between 0 and `decay`.
+    """
+
+    @validate_call
+    def __init__(self, decay: ZeroToOne):
         self.decay = decay
 
     @override
-    def __call__(self, dots: Tensor) -> Tensor:
-        return dots * self.decay
+    def __call__(self, alignment: Tensor) -> Tensor:
+        return alignment * self.decay
 
     def __repr__(self):
-        return f'{type(self).__name__}(decay={self.decay})'
+        return f'{type(self).__name__}(decay={self.decay:.2g})'
+
+    def __str__(self):
+        return f'd*{self.decay:.2g}'
 
 
 class Power(Falloff):
-    """Produces a specular-like lobe."""
+    """
+    Power falloff: d^power
 
+    Produces a specular-like lobe.
+    """
+
+    @validate_call
     def __init__(self, power: float = 10):
         self.power = power
 
     @override
-    def __call__(self, dots: Tensor) -> Tensor:
-        return dots**self.power
+    def __call__(self, alignment: Tensor) -> Tensor:
+        return alignment**self.power
 
     def __repr__(self):
         return f'{type(self).__name__}({self.power})'
+
+    def __str__(self):
+        return f'd^{self.power:.2g}'
--- src/ex_color/intervention/repulsion.py
+++ src/ex_color/intervention/repulsion.py
@@ -32,7 +32,7 @@ class Repulsion(Intervention):
         dots = torch.sum(activations * self.subject[None, :], dim=1)  # [B]
 
         # Scale dot products with falloff function
-        target_dots = dots - self.falloff(dots)  # [B]
+        target_dots = self.falloff(dots)  # [B]
 
         # Decompose into parallel and perpendicular components
         v_parallel = dots[:, None] * self.subject[None, :]  # [B, E]
