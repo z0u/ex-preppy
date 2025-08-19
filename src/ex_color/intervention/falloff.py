ee492bf8894665acc7c8ce5d84b3ff7dcbe98ace
Alex Fraser <alex.d.fraser@gmail.com>
2025-08-19 12:00:26 +0000

    using Bezier mapping for repulsion intervention

--- src/ex_color/intervention/falloff.py
+++ src/ex_color/intervention/falloff.py
@@ -1,9 +1,4 @@
-from typing import Annotated, override
-
-from pydantic import Field, validate_call
 from torch import Tensor
-
-ZeroToOne = Annotated[float, Field(ge=0, le=1)]
 
 
 class Falloff:
@@ -20,47 +15,3 @@ class Falloff:
     """
 
     def __call__(self, alignment: Tensor, /) -> Tensor: ...
-
-
-class Linear(Falloff):
-    """
-    Linear decay: d.decay
-
-    For cosine distance: produces a circular lobe between 0 and `decay`.
-    """
-
-    @validate_call
-    def __init__(self, decay: ZeroToOne):
-        self.decay = decay
-
-    @override
-    def __call__(self, alignment: Tensor) -> Tensor:
-        return alignment * self.decay
-
-    def __repr__(self):
-        return f'{type(self).__name__}(decay={self.decay:.2g})'
-
-    def __str__(self):
-        return f'd*{self.decay:.2g}'
-
-
-class Power(Falloff):
-    """
-    Power falloff: d^power
-
-    Produces a specular-like lobe.
-    """
-
-    @validate_call
-    def __init__(self, power: float = 10):
-        self.power = power
-
-    @override
-    def __call__(self, alignment: Tensor) -> Tensor:
-        return alignment**self.power
-
-    def __repr__(self):
-        return f'{type(self).__name__}({self.power})'
-
-    def __str__(self):
-        return f'd^{self.power:.2g}'
